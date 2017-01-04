74df4f7754ef960beb9ec5ba151a9e5377d6c670
Peter Hozak <peter.hozak@merck.com>
2017-01-04 20:30:50 +0100

    asteroids: acceleration and key handlers

--- asteroids/asteroids1_01.py
+++ asteroids/asteroids1_01.py
@@ -1,7 +1,10 @@
 import pyglet
+import math
 
 WIDTH = 800
 HEIGHT = 600
+ACCELERATION = 300
+ROTATION_SPEED = 200
 
 spaceship_img = pyglet.image.load('playerShip1_blue.png')
 spaceship_img.anchor_x = spaceship_img.width // 2
@@ -18,8 +21,19 @@ def draw():
     window.clear()
     batch.draw()
 
+
+def key_press(key, modificator):
+    pressed_keys.add(key)
+
+
+def key_release(key, modificator):
+    pressed_keys.discard(key)
+
+
 window.push_handlers(
-    on_draw=draw
+    on_draw=draw,
+    on_key_press=key_press,
+    on_key_release=key_release
 )
 
 
@@ -27,14 +41,22 @@ class Spaceship:
     def __init__(self, window):
         self.x = window.width // 2
         self.y = window.height // 2
-        self.x_speed = -20  # TODO: zmen na 0 po implementacii klaves
+        self.x_speed = 0
         self.y_speed = 0
         self.rotation = 0
         self.window = window
         self.sprite = pyglet.sprite.Sprite(spaceship_img, batch=batch)
 
     def tick(self, dt):
-        # TODO: podivej se do pressed_keys
+        if pyglet.window.key.UP in pressed_keys:
+            rotation_radians = math.radians(self.rotation)
+            self.x_speed += dt * ACCELERATION * math.cos(rotation_radians)
+            self.y_speed += dt * ACCELERATION * math.sin(rotation_radians)
+        if pyglet.window.key.LEFT in pressed_keys:
+            self.rotation += dt * ROTATION_SPEED
+        if pyglet.window.key.RIGHT in pressed_keys:
+            self.rotation -= dt * ROTATION_SPEED
+
         self.x += self.x_speed * dt
         self.y += self.y_speed * dt
 
