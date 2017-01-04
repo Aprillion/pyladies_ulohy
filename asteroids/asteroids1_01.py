0396c3fc7069bf6907d22bb07cbf08479351c676
Peter Hozak <peter.hozak@merck.com>
2017-01-04 19:54:48 +0100

    asteroids: tick

--- asteroids/asteroids1_01.py
+++ asteroids/asteroids1_01.py
@@ -7,6 +7,7 @@ spaceship_img = pyglet.image.load('playerShip1_blue.png')
 spaceship_img.anchor_x = spaceship_img.width // 2
 spaceship_img.anchor_y = spaceship_img.height // 2
 
+pressed_keys = set()
 objects = []
 batch = pyglet.graphics.Batch()
 
@@ -34,6 +35,18 @@ class Spaceship:
         self.sprite.x = self.x
         self.sprite.y = self.y
         self.sprite.rotation = 90 - self.rotation
+
+    def tick(self, dt):
+        # TODO: podivej se do pressed_keys
+        pass
+
+
+def tick_all(dt):
+    for obj in objects:
+        obj.tick(dt)
+
+pyglet.clock.schedule(tick_all)
+
 
 objects.append(Spaceship(window))
 
