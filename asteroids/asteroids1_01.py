52e8931c7399bb4774087d77e574ee38af6e5fc3
Peter Hozak <peter.hozak@merck.com>
2017-01-04 18:11:49 +0100

    asteroids: draw handler

--- asteroids/asteroids1_01.py
+++ asteroids/asteroids1_01.py
@@ -3,6 +3,18 @@ import pyglet
 WIDTH = 800
 HEIGHT = 600
 
+objects = []
+batch = pyglet.graphics.Batch()
+
 window = pyglet.window.Window(width=WIDTH, height=HEIGHT)
+
+
+def draw():
+    window.clear()
+    batch.draw()
+
+window.push_handlers(
+    on_draw=draw
+)
 
 pyglet.app.run()
